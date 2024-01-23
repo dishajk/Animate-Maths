2bb306810f5cd09badbe5230f6f6bbe9ba1ce9be
Disha Kuzhively <disha@juny-macbook-pro.icts.res.in>
2024-01-23 18:08:49 +0530

    editing Bhaskara II

--- BhaskaraII_ICTS.py
+++ BhaskaraII_ICTS.py
@@ -5,15 +5,10 @@ class BhaskaraII_ICTS(Scene):
         square_side_length = 4
         tilt_angle = PI / 3
 
-        # A = ORIGIN
-        # B = A + square_side_length * np.array([np.cos(tilt_angle), np.sin(tilt_angle), 0])
-        # C = B + square_side_length * np.array([np.cos(tilt_angle), np.sin(tilt_angle),0])
-
-        # triangle = Polygon(A,B,C)
-        # self.play(Create(triangle))
 
         square3 = Square(side_length=square_side_length, color=GREEN).rotate(tilt_angle)
         self.play(FadeIn(square3))
+        bottom_left_coord = square3.get_corner(DL)
 
         labels = VGroup(
             Tex("c").move_to(square3.get_corner(UL)+DOWN*1.6+RIGHT*0.7),
@@ -21,6 +16,13 @@ class BhaskaraII_ICTS(Scene):
             Tex("c").move_to(square3.get_corner(DR)+UP*1.6+LEFT*0.7),
             Tex("c").move_to(square3.get_corner(UR)+DOWN*0.7+LEFT*1.6),
         )
-
         self.play(FadeIn(labels))
         self.wait(2)
+
+
+        # A = ORIGIN
+        # B = A + square_side_length * np.array([np.cos(tilt_angle), np.sin(tilt_angle), 0])
+        # C = B + square_side_length * np.array([np.cos(tilt_angle), np.sin(tilt_angle),0])
+
+        # triangle = Polygon(A,B,C)
+        # self.play(Create(triangle))
