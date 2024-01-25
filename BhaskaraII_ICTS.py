1028807f5c317dc6434c14e595103055180d9d88
Disha Kuzhively <disha@Apples-MacBook-Pro.local>
2024-01-26 00:53:47 +0530

    adding angles and labels

--- BhaskaraII_ICTS.py
+++ BhaskaraII_ICTS.py
@@ -5,12 +5,15 @@ class BhaskaraII_ICTS(Scene):
         white="#fafafa"
         self.camera.background_color = white
         icts_maroon = "#90403d"
+        icts_highlight="#F88826"
+        icts_grey="#9BAFC5"
         a_length = 8/4
         b_length = 15/4  
 
-        A = [-(a_length+b_length)/2,-(b_length-a_length)/2,0]
+        A = np.array([-(a_length+b_length)/2,-(b_length-a_length)/2,0])
         B = A + a_length*RIGHT
         C = B + b_length*UP
+        mA = -A
 
         triangle = Polygon(A,B,C,stroke_color=white,fill_color=icts_maroon,fill_opacity=1,stroke_width=2)
         triangle2 = triangle.copy().rotate(PI/2).shift(DOWN*(a_length+b_length)/2+RIGHT*(b_length-a_length)/2)
@@ -21,6 +24,10 @@ class BhaskaraII_ICTS(Scene):
         line2 = line1.copy().rotate(-PI/2,about_point=A)
         line3 = line2.copy().rotate(-PI/2,about_point=line2.get_end())
         line4 = line1.copy().rotate(PI/2,about_point=C)
+
+        # bracel1 = Brace(line1,direction=line1.copy().rotate(PI / 2).get_unit_vector(),color=ORANGE,buff=0.1)
+        
+        # c_label = bracel1.get_text("$c$").set_color(BLACK)
         c_label = Tex("$c$",color=BLACK).next_to(line1.get_center(),LEFT)
         c_label2 = Tex("$c$",color=BLACK).next_to(line2.get_center(),DOWN)
         c_label3 = Tex("$c$",color=BLACK).next_to(line3.get_center(),RIGHT)
@@ -31,9 +38,36 @@ class BhaskaraII_ICTS(Scene):
         rightangles3 = RightAngle(line4, line3,stroke_width=4)
         rightangles4 = RightAngle(line1, line4,stroke_width=4,quadrant=(-1,-1))
 
-        linea1 = Line(A,A+RIGHT*b_length,color=BLUE)
+        linea1 = Line(A,A+RIGHT*b_length,color=icts_highlight)
+        linea2 = Line(C,C+DOWN*b_length,color=icts_highlight)
+        linea3 = Line(mA,mA+LEFT*b_length,color=icts_highlight)
+        linea4 = Line(-C,-C+UP*b_length,color=icts_highlight)
+        a_label = Tex("$a$",color=icts_highlight).next_to(linea1.get_center(),DOWN)
+        a_label2 = Tex("$a$",color=icts_highlight).next_to(linea2.get_center(),LEFT)
+        a_label3 = Tex("$a$",color=icts_highlight).next_to(linea3.get_center(),UP)
+        a_label4 = Tex("$a$",color=icts_highlight).next_to(linea4.get_center(),RIGHT)
+    
+        lineb1 = Line(A,A+RIGHT*a_length,color=icts_grey)
+        b_label = Tex("$b$", color=icts_grey).next_to(lineb1.get_center(),UP)
+        lineb2 = Line(C,C+DOWN*a_length,color=icts_grey)
+        b_label2 = Tex("$b$", color=icts_grey).next_to(lineb2.get_center(),RIGHT)
+        lineb3 = Line(-A,-A+LEFT*a_length,color=icts_grey)
+        b_label3 = Tex("$b$", color=icts_grey).next_to(lineb3.get_center(),DOWN)
+        lineb4 = Line(-C,-C+UP*a_length,color=icts_grey)
+        b_label4 = Tex("$b$", color=icts_grey).next_to(lineb4.get_center(),LEFT)
+
+        rightangles11 = RightAngle(lineb1,linea2,stroke_width=3,quadrant=(-1,-1))
+        rightangles22 = RightAngle(lineb4,linea1,stroke_width=3,quadrant=(-1,-1))
+        rightangles33 = RightAngle(lineb3,linea4,stroke_width=3,quadrant=(-1,-1))
+        rightangles44 = RightAngle(lineb2,linea3,stroke_width=3,quadrant=(-1,-1))
+
+        t1 = Angle(lineb1, line1, radius=0.4,color=icts_grey)
+        t2 = Angle(line1, linea2, radius=0.4,color=icts_highlight,quadrant=(-1,1))
+        t1_label = MathTex(r"90^{\circ}-\theta",font_size=24,color=icts_grey).next_to(t1,UR,buff=0)
+        t2_label = MathTex(r"\theta",font_size=24,color=icts_highlight).next_to(t2,DOWN,buff=0.1)
+
         # self.play(FadeIn(triangle),FadeIn(triangle2),FadeIn(triangle3),FadeIn(triangle4))
-        self.add(triangle,triangle2,triangle3,triangle4,rightangles1,rightangles2,rightangles4,rightangles3,line1,line2,line3,line4,c_label,c_label2,c_label3,c_label4,linea1,linea2)
+        self.add(triangle,triangle2,triangle3,triangle4,rightangles1,rightangles2,rightangles4,rightangles3,line1,line2,line3,line4,c_label,c_label2,c_label3,c_label4,linea1,a_label,a_label2,a_label3,a_label4,b_label,lineb2,b_label2,lineb3,b_label3,lineb4,b_label4,rightangles11,t2,t2_label)
         self.wait(2)
 
 
