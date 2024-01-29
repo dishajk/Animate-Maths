15c18f4b43e0c627d10f587760df18bb94f8bd64
Disha Kuzhively <disha@192.168.1.14>
2024-01-29 23:32:11 +0530

    adding animation

--- BhaskaraII_ICTS.py
+++ BhaskaraII_ICTS.py
@@ -72,8 +72,30 @@ class BhaskaraII_ICTS(Scene):
         t1_label = MathTex(r"90^{\circ}-\theta",font_size=28,color=icts_grey).next_to(t1,UR,buff=0)
         t2_label = MathTex(r"\theta",font_size=28,color=icts_highlight).next_to(t2,DOWN,buff=0.1)
 
-        # self.play(FadeIn(triangle),FadeIn(triangle2),FadeIn(triangle3),FadeIn(triangle4))
-        self.add(triangle,triangle2,triangle3,triangle4,rightangles1,rightangles2,rightangles4,rightangles3,line1,line2,line3,line4,c_label,c_label2,c_label3,c_label4,linea1,a_label,a_label2,a_label3,a_label4,b_label,lineb2,b_label2,lineb3,b_label3,lineb4,b_label4,rightangles11,t2,t2_label,t1,t1_label,t3,t4,t5,t6,t7,t8)
+        csquare = Tex("$c^2$",color=BLACK).shift(DOWN*2.9 + LEFT)
+
+        self.play(FadeIn(triangle),FadeIn(triangle2),FadeIn(triangle3),FadeIn(triangle4))
+        self.wait(1)
+        self.play(FadeIn(line1),FadeIn(c_label))
+        self.play(FadeIn(line2),FadeIn(c_label2))
+        self.play(FadeIn(rightangles1))
+        self.play(FadeIn(line3),FadeIn(c_label3))
+        self.play(FadeIn(rightangles2))
+        self.play(FadeIn(line4),FadeIn(c_label4))
+        self.play(FadeIn(rightangles3))
+        self.play(FadeIn(rightangles4))
+        self.play(FadeOut(rightangles1),FadeOut(rightangles2),FadeOut(rightangles4),FadeOut(rightangles3))
+        self.play(ReplacementTransform(c_label,csquare),ReplacementTransform(c_label2,csquare),ReplacementTransform(c_label3,csquare),ReplacementTransform(c_label4,csquare),ReplacementTransform(line1,csquare),ReplacementTransform(line2,csquare),ReplacementTransform(line3,csquare),ReplacementTransform(line4,csquare))
+        self.add(csquare)
+        self.play(Create(lineb1))
+        self.play(FadeIn(b_label))
+        self.play(Create(linea2))
+        self.play(FadeIn(a_label2))
+        self.play(FadeIn(rightangles11))
+        self.play(FadeOut(b_label),FadeOut(a_label2))
+        self.play(FadeIn(t1), FadeIn(t1_label), FadeIn(t2), FadeIn(t2_label))
+        self.wait(1)
+        # self.add(triangle,triangle2,triangle3,triangle4,rightangles1,rightangles2,rightangles4,rightangles3,line1,line2,line3,line4,c_label,c_label2,c_label3,c_label4,linea1,a_label,a_label2,a_label3,a_label4,b_label,lineb2,b_label2,lineb3,b_label3,lineb4,b_label4,rightangles11,t2,t2_label,t1,t1_label,t3,t4,t5,t6,t7,t8)
         # self.wait(2)
 
 
@@ -82,4 +104,4 @@ class BhaskaraII_ICTS(Scene):
         # C = B + square_side_length * np.array([np.cos(tilt_angle), np.sin(tilt_angle),0])
 
         # triangle = Polygon(A,B,C)
-        # self.play(Create(triangle))
+        # self.play(Create(triangle))q
